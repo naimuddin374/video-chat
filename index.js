410f8e0a419f0ef7aa3a1354104aad7510798e5c
root <root@localhost.localdomain>
2021-08-18 20:13:11 +0000

    Add build command

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 
 
 // PRODUCTION ENV   
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
     app.use(express.static(__dirname + "/client/build"))
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname + "/client/build/index.html"))
