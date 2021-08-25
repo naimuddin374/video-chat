a990c749dcbe658280339628c72febabe862e3fd
Naim Uddin <naim@ticonsys.com>
2021-08-26 01:36:14 +0600

    Change connection url

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,7 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:4000');
+// const socket = io('http://localhost:4000');
+const socket = io('https://lubyc-video-chat.herokuapp.com');
 
 
 const ContextProvider = ({ children }) => {
